0367c196f76ae4b75d7d53c10cfe318ae8640da0
H_Khalid__H <khalidjj78200@icloud.com>
2025-01-10 01:25:22 +0100

    galere pour rechargement automatique des livre avec ajax

--- src/Controller/BookReadController.php
+++ src/Controller/BookReadController.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\BookRead;
-use App\Entity\Book;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class BookReadController extends AbstractController
 {
-    #[Route('/', name: 'book_read_add', methods: ['POST'])]
-    public function addBookRead(Request $request, EntityManagerInterface $entityManager, BookRepository $bookRepository): Response
+    #[Route('/add-book-read', name: 'book_read_add', methods: ['POST'])]
+    public function addBookRead(Request $request, EntityManagerInterface $entityManager, BookRepository $bookRepository): JsonResponse
     {
-        // Récupérer les données du formulaire
         $bookId = $request->request->get('book');
         $description = $request->request->get('description');
         $rating = $request->request->get('rating');
@@ -26,9 +26,8 @@ class BookReadController extends AbstractController
             return $this->json(['error' => 'Book not found'], Response::HTTP_NOT_FOUND);
         }
 
-        // Créer une nouvelle instance de BookRead
         $bookRead = new BookRead();
-        $bookRead->setUserId($this->getUser()->getId()); 
+        $bookRead->setUserId($this->getUser()->getId());
         $bookRead->setBook($book);
         $bookRead->setDescription($description);
         $bookRead->setRating($rating);
@@ -36,10 +35,15 @@ class BookReadController extends AbstractController
         $bookRead->setCreatedAt(new \DateTime());
         $bookRead->setUpdatedAt(new \DateTime());
 
-        // Persister et sauvegarder
         $entityManager->persist($bookRead);
         $entityManager->flush();
 
-        return $this->json(['message' => 'Book read added successfully'], Response::HTTP_OK);
+        return $this->json([
+            'id' => $bookRead->getId(),
+            'bookName' => $book->getName(),
+            'description' => $description,
+            'rating' => $rating,
+            'isRead' => $isRead
+        ], Response::HTTP_OK);
     }
 }
